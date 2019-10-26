544e2beb9015c98f1c882cbd47c2dc70e3664dad
glisselisbethjm <glisse1986.jm@outlook.com>
2019-10-26 03:26:02 -0500

    Updates

---
+++ precache-manifest.cdc5c3ab7ac679faf832e39e2958d6f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd95de794b70a413f58f440f318acaa3",
+    "url": "/proyecto-noticias/index.html"
+  },
+  {
+    "revision": "6c45bfae97af25f19572",
+    "url": "/proyecto-noticias/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "98e7edc5b72990a4534a",
+    "url": "/proyecto-noticias/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6c45bfae97af25f19572",
+    "url": "/proyecto-noticias/static/js/2.6cdd16db.chunk.js"
+  },
+  {
+    "revision": "98e7edc5b72990a4534a",
+    "url": "/proyecto-noticias/static/js/main.95d64275.chunk.js"
+  },
+  {
+    "revision": "966b7afb410a91509761",
+    "url": "/proyecto-noticias/static/js/runtime-main.0d0fd50b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/proyecto-noticias/static/media/logo.25bf045c.svg"
+  }
+]);
